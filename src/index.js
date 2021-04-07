5ff77e140804c2c602faad37df3aae8208f9e17a
hodacbaoluu <luuhodacbao@gmail.com>
2021-04-07 20:42:39 +0700

    upcode

--- src/index.js
+++ src/index.js
@@ -3,7 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+//bootstrap
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.min.js";
+import "../node_modules/jquery/dist/jquery.min.js";
+import "../node_modules/popper.js/dist/popper.min.js"
+// document.getElementById("root").innerHTML = <App />
 ReactDOM.render(
   <React.StrictMode>
     <App />
